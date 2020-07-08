b4a8b4f3ad64216520a65e3e0cba9637ffd90979
Brendan Abernethy <brendan.j.abernethy@gmail.com>
2020-07-08 13:40:39 -0500

    added nav and routing

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,42 +1,15 @@
 import React from "react";
-import AnimalCard from "./animal/AnimalCard";
-import EmployeeCard from "./employee/EmployeeCard";
-import OwnerCard from "./owner/OwnerCard";
-import LocationCard from "./location/LocationCard";
+import NavBar from "./nav/NavBar";
+import ApplicationViews from "./ApplicationViews";
+
 import "./Kennel.css";
 
 const Kennel = () => {
   return (
-    <div>
-      <div>
-        <h2>
-          Student Kennels
-          <br />
-          <small>Loving care when you're not there.</small>
-        </h2>
-        <address>
-          Visit Us at the Nashville North Location
-          <br />
-          500 Puppy Way
-        </address>
-      </div>
-      <div className="container-cards">
-        <LocationCard />
-      </div>
-      <div className="container-cards">
-        <AnimalCard />
-        <AnimalCard />
-        <AnimalCard />
-      </div>
-      <div className="container-cards">
-        <EmployeeCard />
-        <EmployeeCard />
-        <EmployeeCard />
-      </div>
-      <div className="container-cards">
-        <OwnerCard />
-      </div>
-    </div>
+    <>
+      <NavBar />
+      <ApplicationViews />
+    </>
   );
 };
 
